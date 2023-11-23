87dffc75479f4fc08319a08ae5ea6493d25b388d
ying.andrews <ying.andrews@gmail.com>
2023-11-22 19:18:40 -0500

    Added two new UI components for adding and searching for appointments. They are dummies now.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
-import { BiArchive } from "react-icons/bi";
+import { BiCalendar } from "react-icons/bi";
+import Search from "./components/Search";
+import AddAppointment from "./components/AddAppointment";
 
 function App() {
   return (
     <div className="App container mx-auto mt-3 font-thin">
-      <h1 className="text-5xl"> <BiArchive className="inline-block text-blue-800 align-top"/> Your Appointments</h1>
+      <h1 className="text-5xl mb-3"> 
+        <BiCalendar className="inline-block text-blue-800 align-top"/> 
+        Your Appointments
+      </h1>
+      <AddAppointment />
+      <Search />
     </div>
   );
 }
