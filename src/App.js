9bd3f9d201cfd76493ec41a5171b61d1e941d384
ying.andrews <ying.andrews@gmail.com>
2023-11-24 19:06:14 -0500

    Implemented 'Sort By' feature

--- src/App.js
+++ src/App.js
@@ -6,6 +6,31 @@ import AppointmentInfo from "./components/AppointmentInfo";
 
 function App() {
   let [appointmentList, setAppointmentList] = useState([]);
+  let [query, setQuery] = useState("");
+  let [sortBy, setSortBy] = useState("petName");
+  let [orderBy, setOrderBy] = useState('asc');
+
+  const filteredAppointments = appointmentList.filter(
+    item => {
+      return (
+        item.petName.toLowerCase().includes(query.toLowerCase()) ||
+        item.ownerName.toLowerCase().includes(query.toLowerCase()) ||
+        item.aptNotes.toLowerCase().includes(query.toLowerCase()) 
+      );
+    }
+  ).sort(
+    (a, b) => {
+      let order = (orderBy === 'asc') ? 1 : -1;
+      if (sortBy !== 'appDate')
+        return (
+          a[sortBy].toLowerCase() < b[sortBy].toLowerCase() ? -1 * order : 1 * order
+        );
+      else
+        return (
+          a[sortBy] < b[sortBy] ? -1 * order : 1 * order
+        );
+    }
+  );
 
   const fetchData = useCallback(
     () => {
@@ -24,10 +49,16 @@ function App() {
         Pet Appointments
       </h1>
       <AddAppointment />
-      <Search />
+      <Search query={query}
+        onQueryChange={myQuery => setQuery(myQuery)}
+        sortBy={sortBy}
+        onSortByChange={mySort => setSortBy(mySort)}
+        orderBy={orderBy}
+        onOrderByChange={myOrder => setOrderBy(myOrder)}  
+      />
 
       <ul className="divide-y divide-gray-200">
-        {appointmentList
+        {filteredAppointments
           .map(appointment => (
             <AppointmentInfo key={appointment.id}
               appointment={appointment}
